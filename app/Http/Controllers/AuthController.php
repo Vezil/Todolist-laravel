49059d6a64e26b9111982690c3a34c788c1242ef
Vezil <SzymonWojaczek@wp.pl>
2019-12-10 19:36:22 +0100

    logout functionality

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,7 +6,7 @@ use App\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
-// use Guzzle\Http\Client;
+
 
 
 use Laravel\Passport\Http\Controllers\AccessTokenController;
@@ -20,17 +20,7 @@ class AuthController extends Controller
 {
     public function login(Request $request)
     {
-    //    $http = new \GuzzleHttp\Client;
 
-    //    try{
-    //         $response = $http->post(config('services.passport.login_endpoint'), [
-    //             'form_params' => [
-    //                 'grant_type' => 'password',
-    //                 'client_id' => config('services.passport.client_id'),
-    //                 'client_secret' => config('services.passport.client_secret'),
-    //                 'username' => $request->username,
-    //                 'password' => $request->password,]
-    //             ]);
             $request->request->add([
                 'grant_type' => 'password',
                 'client_id' => env('PASSPORT_CLIENT_ID'),
@@ -47,24 +37,7 @@ class AuthController extends Controller
             return $response;
         } 
 
-           
-       //  return $response->getBody();
-        //  catch(error $e) {return 'response($e)';}
-             
-    //     } catch (\GuzzleHttp\Exception\BadResponseException $e) {
-    //         if($e->getCode() === 400) {
 
-    //             return response()->json('Invaild Request. Please enter a username or a password', $e->getCode());
-
-    //         } else if ($e->getCode() === 401){
-
-    //             return response()->json('Invaild Request. Please enter a correct username or password', $e->getCode());
-
-    //         }
-
-    //         return response()->json('Something went wrong on the server', $e->getCode());
-    //     }
-    // }
 
     public function register(Request $request)
     {
@@ -79,5 +52,12 @@ class AuthController extends Controller
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
+    }
+    public function logout()
+    {
+        auth()->user()->tokens->each(function ($token, $key) {
+            $token->delete();
+        });
+        return response()->json('Logged out successfully', 200);
     }
 }
