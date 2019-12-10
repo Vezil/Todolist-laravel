0da645053947232762ccd0e342f34376f31aeff1
Vezil <SzymonWojaczek@wp.pl>
2019-12-10 03:11:32 +0100

    Login and Register validations

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,9 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::post('/login', 'AuthController@login');
+Route::post('/register', 'AuthController@register');
+
 Route::get('/todos','TodosController@index');
 Route::post('/todos','TodosController@store');
 Route::patch('/todos/{todo}','TodosController@update');
